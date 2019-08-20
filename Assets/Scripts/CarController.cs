51bde4afa1334f17929d72bba1ba1bdf49155be0
Arash Tadjiki <atadjiki@gmail.com>
2019-08-20 15:30:16 -0600

    working on car

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+    public GameObject Thrust_Back_Left;
+    public GameObject Thrust_Back_Right;
+    public GameObject Thrust_Front_Left;
+    public GameObject Thrust_Front_Right;
+    public GameObject Thrust_Main;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

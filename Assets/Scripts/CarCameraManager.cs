27478d6f024d235d33f76bbb68c2fe9f99005eaa
Arash Tadjiki <atadjiki@gmail.com>
2019-08-22 13:00:07 -0600

    working on camera shit

--- Assets/Scripts/CarCameraManager.cs
+++ Assets/Scripts/CarCameraManager.cs
@@ -6,49 +6,37 @@ using UnityEngine;
 public class CarCameraManager : MonoBehaviour
 {
 
-    private Rigidbody rb;
-    public float x_axis_threshold;
-    public float y_axis_threshold; //at what point we decide to switch cameras from the standard back camera
+    private HoverControlScheme controls;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        controls = GetComponent<HoverControlScheme>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        CameraRigManager.Camera Camera = CameraRigManager.Camera.Back;
-
-        //set camera based on the velocity of the car
-        float x = rb.velocity.x; //left and right turning
-        float y = rb.velocity.y; //up and down turning
-        float z = rb.velocity.z; //this will typically be the largest value is the car is moving
-
-
-        if (Mathf.Abs(x) > x_axis_threshold)
+        
+        if(controls.turn_input == HoverControlScheme.InputState.Positive)
         {
-            if (x < 0)
-            {
-                Camera = CameraRigManager.Camera.Right;
-            }
-            else
-            {
-                Camera = CameraRigManager.Camera.Left;
-            }
-
+            CameraRigManager.Instance.SwitchTo(CameraRigManager.Camera.Left);
+        }
+        else if (controls.turn_input == HoverControlScheme.InputState.Negative)
+        {
+            CameraRigManager.Instance.SwitchTo(CameraRigManager.Camera.Right);
+        }
+        else if (controls.ascend_input == HoverControlScheme.InputState.Positive)
+        {
+            CameraRigManager.Instance.SwitchTo(CameraRigManager.Camera.Down);
+        }
+        else if (controls.ascend_input == HoverControlScheme.InputState.Negative)
+        {
+            CameraRigManager.Instance.SwitchTo(CameraRigManager.Camera.Up);
         }
         else
         {
-            if (Mathf.Abs(z) > Mathf.Abs(x) && Mathf.Abs(z) > Mathf.Abs(y))
-            {
-                Camera = CameraRigManager.Camera.Back;
-            }
-
+            CameraRigManager.Instance.SwitchTo(CameraRigManager.Camera.Back);
         }
-
-        CameraRigManager.Instance.SwitchTo(Camera);
-
 
     }
 }

7b0e07c18605856c13fdf7b2c232da0604ea736a
Rohan Patel <rohanpatel1899@gmail.com>
2019-08-27 12:24:10 -0600

    cutscene added

---
+++ Assets/CameraCollidersManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollidersManager : MonoBehaviour
+{
+    public BoxCollider aferDolly;
+
+
+    public static CameraCollidersManager Instance;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+
+    public void handleCameraSwitching(collideNotifier cn)
+    {
+        if(cn.ID==1)
+        {
+            CameraRigManager.Instance.SwitchTo(CameraRigManager.Camera.Back);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/collideNotifier.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collideNotifier : MonoBehaviour
+{
+
+    public float ID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if(other.gameObject.name.Contains("CarChassis"))
+        {
+            CameraCollidersManager.Instance.handleCameraSwitching(this);
+        }
+    }
+}
